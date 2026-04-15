ec15f67d6df7f02ff7ee95f5f0bf75f89e07c4f5
Sandeep Kumar K <sandeepsandeep96512@gmail.com>
2026-04-15 14:23:52 +0530

    Update script_ubuntu.sh

--- script_ubuntu.sh
+++ script_ubuntu.sh
@@ -1,19 +1,7 @@
-#!/bin/bash
-
-set -e
-
-sudo apt-get update -y
-sudo apt-get upgrade -y
-sudo apt-get install -y xfce4 xfce4-goodies
-sudo apt-get install -y xrdp
-
-echo "xfce4-session" > /etc/xrdp/startwm.sh
-sudo chown -R xrdp:xrdp /home/xrdp
-sudo chown -R xrdp:xrdp /var/run/xrdp
-
-sudo systemctl start xrdp
-sudo systemctl enable xrdp
-
-sudo ufw allow 3389/tcp 2>/dev/null || true
-
-echo "XRDP installation complete. Connect to port 3389."
+sudo apt update
+sudo apt install -y gnome-remote-desktop dbus-x11
+sudo loginctl enable-linger nitest
+sudo -u nitest dbus-launch gsettings set org.gnome.desktop.remote-desktop.rdp enable true
+sudo -u nitest dbus-launch gsettings set org.gnome.desktop.remote-desktop.rdp view-only false
+sudo -u nitest dbus-launch gsettings set org.gnome.desktop.remote-desktop.rdp screen-share-mode 'extend'
+sudo ufw allow 3389/tcp
